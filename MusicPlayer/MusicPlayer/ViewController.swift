6c2d9e06c4cc15d166ea07b9d1bb3149e076d57f
임태완 <ltw2001@hanmail.net>
2019-07-31 17:45:13 +0900

    MusicPlayer Update

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 임태완 on 2019. 7. 31..
+//  Copyright © 2019년 임태완. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    // MARK : IBOutlets
+    @IBOutlet var playPauseButton : UIButton!
+    @IBOutlet var timeLabel : UILabel!
+    @IBOutlet var progressSlider : UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
